44d63a17a080aca9b36ca6a67a826ba692ee150f
Domenico Nappo <domenico.nappo@gmail.com>
2018-04-30 15:32:38 +0200

    forcing update image in docker stack deploy

--- up.sh
+++ up.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 docker-compose config > docker-compose-parsed.yaml
 docker stack deploy -c ./docker-compose-parsed.yaml SMFR
+docker service update SMFR_geonames
+docker service update SMFR_annotator
+docker service update SMFR_geocoder
+docker service update SMFR_restserver
+docker service update SMFR_web
