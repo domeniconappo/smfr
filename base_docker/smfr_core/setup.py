93b527dfc21ca296a24fb27edd3d7696acf20d80
Domenico Nappo <domenico.nappo@gmail.com>
2018-05-11 20:59:52 +0200

    new cassandra image with jmx enabled properly

--- base_docker/smfr_core/setup.py
+++ base_docker/smfr_core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='smfrcore',
-    version='1.1',
+    version='1.1.1',
     packages=find_packages(),
     description='SMFR Core modules (models, utilities)',
     author='Domenico Nappo',
