96d9b174d2fe93279c1fc4b8eea1cdd6334dd674
Domenico Nappo <domenico.nappo@gmail.com>
2018-06-06 12:50:05 +0200

    change gunicor web startup command; changed format of cassandra annotations map field in export

--- web/scripts/start.sh
+++ web/scripts/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-gunicorn -b 0.0.0.0:8888 --workers 1 --error-logfile - --access-logfile - --reload --name webapp "start:app"
+gunicorn -b 0.0.0.0:8888 --workers 4 --worker-class gevent --reload --name webapp "start:app"
