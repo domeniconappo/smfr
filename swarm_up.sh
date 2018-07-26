959c7ca5374e1acf476671a929f7a11590b7fe42
Domenico Nappo <domenico.nappo@gmail.com>
2018-07-26 15:22:35 +0200

    updated swarm deploy script

--- swarm_up.sh
+++ swarm_up.sh
@@ -10,13 +10,16 @@ function getProperty {
 
 export image_tag=`cat VERSION | grep "VERSION" | cut -d'=' -f2`
 
-docker-compose config > docker-compose-parsed.yaml
 
-# cleaning volumes from docker compose configuration
-python3 compose4deploy.py -i docker-compose-parsed.yaml -o docker-compose-4deploy.yaml
-docker stack deploy -c ./docker-compose-4deploy.yaml SMFR
+DOCKER_ID_USER=$(getProperty "DOCKER_ID_USER")
+DOCKER_ID_PASSWORD=$(getProperty "DOCKER_ID_PASSWORD")
+DOCKER_REGISTRY=$(getProperty "DOCKER_REGISTRY")
 
-# forcing updates of images
+if [ -n "${DOCKER_ID_USER}" ] && [ ${DOCKER_REGISTRY} != "index.docker.io" ]; then
+    echo Pulling from a private registry: ${DOCKER_REGISTRY} - need to login
+    docker login -u ${DOCKER_ID_USER} -p ${DOCKER_ID_PASSWORD} ${DOCKER_REGISTRY}
+fi
+
 PERSISTER_IMAGE=$(getProperty "PERSISTER_IMAGE")
 AGGREGATOR_IMAGE=$(getProperty "AGGREGATOR_IMAGE")
 ANNOTATOR_IMAGE=$(getProperty "ANNOTATOR_IMAGE")
@@ -27,12 +30,32 @@ MYSQL_IMAGE=$(getProperty "MYSQL_IMAGE")
 CASSANDRA_IMAGE=$(getProperty "CASSANDRA_IMAGE")
 GEONAMES_IMAGE=$(getProperty "GEONAMES_IMAGE")
 
-docker service update SMFR_persister --detach=false --image ${PERSISTER_IMAGE}:${image_tag}
-docker service update SMFR_geonames --detach=false --image ${AGGREGATOR_IMAGE}:${image_tag}
-docker service update SMFR_mysql --detach=false --image ${ANNOTATOR_IMAGE}:${image_tag}
-docker service update SMFR_annotator --detach=false --image ${GEOCODER_IMAGE}:${image_tag}
-docker service update SMFR_geocoder --detach=false --image ${RESTSERVER_IMAGE}:${image_tag}
-docker service update SMFR_restserver --detach=false --image ${WEB_IMAGE}:${image_tag}
-docker service update SMFR_web --detach=false --image ${MYSQL_IMAGE}:${image_tag}
-docker service update SMFR_cassandrasmfr --detach=false --image ${CASSANDRA_IMAGE}:${image_tag}
-docker service update SMFR_geonames --detach=false --image ${GEONAMES_IMAGE}:${image_tag}
+docker-compose config > docker-compose-parsed.yaml
+
+# cleaning volumes from docker compose configuration
+python3 compose4deploy.py -i docker-compose-parsed.yaml -o docker-compose-4deploy.yaml
+
+docker pull ${DOCKER_REGISTRY}/${PERSISTER_IMAGE}:${image_tag}
+docker pull ${DOCKER_REGISTRY}/${AGGREGATOR_IMAGE}:${image_tag}
+docker pull ${DOCKER_REGISTRY}/${ANNOTATOR_IMAGE}:${image_tag}
+docker pull ${DOCKER_REGISTRY}/${GEOCODER_IMAGE}:${image_tag}
+docker pull ${DOCKER_REGISTRY}/${RESTSERVER_IMAGE}:${image_tag}
+docker pull ${DOCKER_REGISTRY}/${WEB_IMAGE}:${image_tag}
+docker pull ${DOCKER_REGISTRY}/${MYSQL_IMAGE}:${image_tag}
+docker pull ${DOCKER_REGISTRY}/${CASSANDRA_IMAGE}:${image_tag}
+docker pull ${DOCKER_REGISTRY}/${GEONAMES_IMAGE}:${image_tag}
+
+docker stack deploy -c ./docker-compose-4deploy.yaml SMFR
+
+# forcing updates of images
+
+
+docker service update SMFR_persister --detach=false --image ${DOCKER_REGISTRY}/${PERSISTER_IMAGE}:${image_tag}
+docker service update SMFR_geonames --detach=false --image ${DOCKER_REGISTRY}/${AGGREGATOR_IMAGE}:${image_tag}
+docker service update SMFR_mysql --detach=false --image ${DOCKER_REGISTRY}/${ANNOTATOR_IMAGE}:${image_tag}
+docker service update SMFR_annotator --detach=false --image ${DOCKER_REGISTRY}/${GEOCODER_IMAGE}:${image_tag}
+docker service update SMFR_geocoder --detach=false --image ${DOCKER_REGISTRY}/${RESTSERVER_IMAGE}:${image_tag}
+docker service update SMFR_restserver --detach=false --image ${DOCKER_REGISTRY}/${WEB_IMAGE}:${image_tag}
+docker service update SMFR_web --detach=false --image ${DOCKER_REGISTRY}/${MYSQL_IMAGE}:${image_tag}
+docker service update SMFR_cassandrasmfr --detach=false --image ${DOCKER_REGISTRY}/${CASSANDRA_IMAGE}:${image_tag}
+docker service update SMFR_geonames --detach=false --image ${DOCKER_REGISTRY}/${GEONAMES_IMAGE}:${image_tag}
