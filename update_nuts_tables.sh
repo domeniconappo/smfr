229871ab40264eb321c45ff8e7fd229743da8f0b
Domenico Nappo <domenico.nappo@gmail.com>
2018-09-03 12:33:48 +0200

    convenient sh script to update nuts tables

--- update_nuts_tables.sh
+++ update_nuts_tables.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
+source functions.sh
+
+./singlenode_up.sh mysql
+
+export MYSQL_USER=$(getProperty "MYSQL_USER")
+export MYSQL_PASSWORD=$(getProperty "MYSQL_PASSWORD")
+export MYSQL_DBNAME=$(getProperty "MYSQL_DBNAME")
 
 python scripts/update_smfr_nuts_tables.py
+
+./singlenode_down.sh
