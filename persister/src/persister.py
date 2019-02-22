405972e05945e97c58e2e1d68746e323ea514bd8
Domenico Nappo <domenico.nappo@gmail.com>
2019-02-22 12:39:26 +0100

    added counters

--- persister/src/persister.py
+++ persister/src/persister.py
@@ -112,7 +112,7 @@ class Persister:
                             logger.debug('Saved tweet: %s - collection %d', tweet.tweetid, tweet.collectionid)
 
                         with self._lock:
-                            trigger_key = TwitterCollection.TRIGGER_ONDEMAND if tweet.is_ondemand else TwitterCollection.TRIGGER_BACKGROUND
+                            trigger_key = str(tweet.collection.trigger)
                             self.shared_counter[trigger_key] += 1
                             self.shared_counter[tweet.ttype] += 1
                             self.shared_counter['{}-{}'.format(tweet.lang, tweet.ttype)] += 1
