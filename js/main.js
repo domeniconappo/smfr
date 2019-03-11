392bf2ae24ec94bd77275b639421b71aae223355
Domenico Nappo <domenico.nappo@gmail.com>
2019-03-11 14:12:58 +0100

    added bootstrap fw

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// Off Canvas Sliding
+$(document).ready(function(){
+  // Menu button click
+  $('#js-menu-trigger,#js-menu-screen').on('click touchstart', function(e){
+    // $('#js-body').toggleClass('no-scroll');
+    $('#js-menu, #js-menu-screen').toggleClass('is-visible');
+    $('#js-menu-trigger').toggleClass('slide close');
+    // $('#masthead, #page-wrapper').toggleClass('slide');
+    e.preventDefault();
+  });
+});
+
+// FitVids
+$(document).ready(function(){
+	// Target your .container, .wrapper, .post, etc.
+	$("#main").fitVids();
+	$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+  });
+});
+// Table of Contents title. Change text to localize
+$("#markdown-toc").prepend("<li><h6>{{ site.data.messages.locales[site.locale].overview }}</h6></li>");
