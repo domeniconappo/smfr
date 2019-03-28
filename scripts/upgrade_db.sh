c9c7c106463afcead3578c0109d88a7a78025c48
Domenico Nappo <domenico.nappo@gmail.com>
2019-03-28 16:33:19 +0100

    improved upgrade_db script with more options

--- scripts/upgrade_db.sh
+++ scripts/upgrade_db.sh
@@ -3,6 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${DIR}/export_env.sh
 mode=${1:-compose}
+action=${2:-generate}
 
 if [[ ${mode} == "compose" ]]; then
     ${DIR}/singlenode_dbs.sh
@@ -11,11 +12,22 @@ elif [[ ${mode} == "swarm" ]]; then
 fi
 
 ${DIR}/install_shared_libs.sh models
-
+echo
+echo
+echo "================== RUN MIGRATIONS =================="
 cd ${DIR}/../base/shared_libs/smfr_models/smfrcore/models/sql/migrations
-
-alembic revision --autogenerate
-alembic upgrade head
+echo
+echo
+echo " current revision ------> "
+alembic current
+if [[ ${action} == "generate" ]]; then
+    alembic revision --autogenerate
+elif [[ ${action} == "upgrade" ]]; then
+    alembic upgrade head
+    echo " current revision ------> "
+    alembic current
+fi
+echo "================== END MIGRATIONS =================="
 
 cd -
 echo "[WARN] Databases were started. To stop db services execute ./scripts/singlenode_down.sh or ./scripts/swarm_down.sh"
